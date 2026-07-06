1b5b64001e239178f2fb3661fd659d3d80fdddcf
ARCHIT <architsingh23993@gmail.com>
2026-07-07 05:04:42 +0530

    Mail configuration completed

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,4 +1,51 @@
 import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
+
+
+/* 
+    options:{
+        email: ,
+        subject: ,
+        mailGenContent:
+    }
+*/
+const sendEmail = function(options){
+
+    const mailGenerator=Mailgen({
+        theme: "default",
+        product:{
+            name: "Project Management",
+            link: "https://ProjectManager.example.com"
+        }
+    });
+
+    const emailTextual = mailGenerator.generate(options.mailGenContent);
+    const emailHtml= mailGenerator.generate(options.mailGenContent);
+
+    const transporter=nodemailer.createTransport({
+        host: process.env.SMTP_MAILTRAP_HOST,
+        port: process.env.SMTP_MAILTRAP_PORT,
+        auth:{
+            username: process.env.SMTP_MAILTRAP_USERNAME,
+            password: process.env.SMTP_MAILTRAP_PASSWORD
+        }
+    });
+
+    const mail={
+        from: "projectManagement.gmail.com",
+        to: options.email,
+        subject: options.subject,
+        text: emailTextual,
+        html: emailHtml
+    }
+
+    try{
+        await transporter.sendMail(mail);
+    } catch(error){
+        console.error("Try checking the env variables");
+        console.error("Error: ", error);
+    }
+}
 
 const emailVerificationEmailGenContent = function (username, emailVerificationLink){
     return {
@@ -35,3 +82,5 @@ const forgotPasswordEmailGenContent = function (username, forgetPasswordLink){
         outro: "Need help, or feel stuck? Reply with help on the mail and we will be pleased to help"
     }
 }
+
+export { sendEmail, emailVerificationEmailGenContent, forgotPasswordEmailGenContent };
